c1860ca76c17565fdd16e6e416c1da276abd891f
sedali <seyedaliseyedalian1394@gmail.com>
2017-12-11 16:09:33 +0000

    Necessary files
    round robin:   
    all of you need is in three file.previous uploads must be ignored.
    the function removeCurrentThread runs without yielding thread but with a debug test printing every 100tiks.
    if it can yield the Current thread every 100tiks the round robin is Implemented.

---
+++ sedali/threadtest.cc
@@ -0,0 +1,85 @@
+// threadtest.cc 
+//	Simple test case for the threads assignment.
+//
+//	Create two threads, and have them context switch
+//	back and forth between themselves by calling Thread::Yield, 
+//	to illustratethe inner workings of the thread system.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+#include<unistd.h>
+
+// testnum is set in main.cc
+int testnum = 1;
+
+//----------------------------------------------------------------------
+// SimpleThread
+// 	Loop 5 times, yielding the CPU to another ready thread 
+//	each iteration.
+//
+//	"which" is simply a number identifying the thread, for debugging
+//	purposes.
+//----------------------------------------------------------------------
+
+void
+SimpleThread(int which)
+{
+    int num;
+    
+    for (num = 0; num < 5; num++) {
+    printf("*** thread %d looped %d times\n", which, num);
+        usleep(50);
+        currentThread->Yield();
+    }
+}
+
+//----------------------------------------------------------------------
+// ThreadTest1
+// 	Set up a ping-pong between two threads, by forking a thread 
+//	to call SimpleThread, and then calling SimpleThread ourselves.
+//----------------------------------------------------------------------
+
+void
+ThreadTest1()
+{
+    DEBUG('t', "Entering ThreadTest1");
+
+    Thread *t = new Thread("forked thread");
+    Thread *t1 = new Thread("forked thread");
+    Thread *t2 = new Thread("forked thread");
+    Thread *t3 = new Thread("forked thread");
+    Thread *t4 = new Thread("forked thread");
+    Thread *t5 = new Thread("forked thread");
+    Thread *t6 = new Thread("forked thread");
+
+    t1->Fork(SimpleThread, 1);
+    t2->Fork(SimpleThread, 2);
+    t3->Fork(SimpleThread, 3);
+    t4->Fork(SimpleThread, 4);
+    t5->Fork(SimpleThread, 5);
+    t6->Fork(SimpleThread, 6);
+    SimpleThread(0);
+}
+
+//----------------------------------------------------------------------
+// ThreadTest
+// 	Invoke a test routine.
+//----------------------------------------------------------------------
+
+void
+ThreadTest()
+{
+    switch (testnum) {
+    case 1:
+	ThreadTest1();
+	break;
+    default:
+	printf("No test specified.\n");
+	break;
+    }
+}
+
